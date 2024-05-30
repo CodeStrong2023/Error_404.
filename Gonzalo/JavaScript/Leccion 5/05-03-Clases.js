d67958aa4bf8a937273aa67dbb0653a55b822606
Gonzalo <gonzaloromero2207@gmail.com>
2024-05-30 01:58:04 -0300

    Clase 6 de JavaScript terminada

---
+++ Gonzalo/JavaScript/Leccion 5/05-03-Clases.js
@@ -0,0 +1,58 @@
+//let persona3 = new Persona('Carla','Ponce'); esto no se debe hacer: Persona is not defined
+class Persona{ //Clase padre
+    constructor(nombre,apellido){
+        this._nombre = nombre;
+        this._apellido = apellido;
+    }
+
+    get nombre(){
+        return this._nombre;
+    }
+
+
+    set nombre(nombre){
+        this._nombre = nombre;
+    }
+    get apellido(){
+        return this._apellido;
+    }
+
+
+    set apellido(apellido){
+        this._apellido = apellido;
+    }
+}
+
+class Empleado extends Persona{ //Clase hija
+    constructor(nombre,apellido,departamento){
+        super(nombre,apellido);
+        this._departamento = departamento;
+    }
+    get departamento(){
+        return this._departamento;
+    }
+    set departamento(departamento){
+        this._departamento = departamento;
+    }
+}
+
+let persona1 = new Persona('Martín','Pérez');
+console.log(persona1.nombre);
+persona1.nombre = 'Juan  Carlos';
+console.log(persona1.nombre)
+let persona2 = new Persona('Carlos','Lara');
+console.log(persona2);
+persona2.nombre = 'María Laura';
+console.log(persona2.nombre);
+
+persona1.apellido = 'Ramirez';
+persona2._apellido = 'Martinez';
+console.log(persona1.apellido);
+console.log(persona2.apellido);
+
+
+let empleado1 = new Empleado('María','Gimenez','Sistemas');
+
+
+console.log(empleado1);
+console.log(empleado1.nombre);
